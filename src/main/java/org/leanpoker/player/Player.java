018c7f4ede819171b213ffbb5674fcc500ed11c1
Adrian Polak <adrian.polak.ext@adcubum.com>
2018-10-04 11:16:12 +0200

    1 - Basics

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,14 +1,45 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "1";
 
     public static int betRequest(JsonElement request) {
+        JsonArray communityCards = request.getAsJsonObject().getAsJsonArray("community_cards");
+        int currentBuyIn = request.getAsJsonObject().getAsJsonPrimitive("current_buy_in").getAsInt();
+        int playerInAction = request.getAsJsonObject().getAsJsonPrimitive("in_action").getAsInt();
+        int playerBet = request.getAsJsonObject().getAsJsonArray("players").get(playerInAction)
+              .getAsJsonObject().getAsJsonPrimitive("bet").getAsInt();
+
+        int smallBlind = request.getAsJsonObject().getAsJsonPrimitive("small_blind").getAsInt();
+
+        int bigBlind = smallBlind * 2;
+        int minimalBet = currentBuyIn - playerBet;
+        //Erste Runde
+        if (communityCards.size() == 0) {
+            if(bigBlind == currentBuyIn){
+                return minimalBet;
+            }
+        }
+        //Zweite Runde
+        if (communityCards.size() == 3) {
+            if(bigBlind == currentBuyIn){
+                return minimalBet;
+            }
+        }
+        //Dritte Runde
+        if (communityCards.size() == 4) {
+
+        }
+        //Vierte Runde
+        if (communityCards.size() == 5) {
+
+        }
         return 0;
     }
 
